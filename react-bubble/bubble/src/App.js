c4a9230f365aa0861f999e98f3f1fd0b77ea9f5e
Finder <jewell.finder@parivedasolutions.com>
2018-09-19 16:52:08 -0500

    User React page working

--- react-bubble/bubble/src/App.js
+++ react-bubble/bubble/src/App.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
+
+class User extends Component {
+  render() {
+    return (
+      <div className="User">
+
+        <div className="profContainer">
+            <img className="prof" alt="Profile Pic" src={"https://www.eharmony.com/dating-advice/wp-content/uploads/2017/07/selfcenteredman.png"}/>
+        </div>
+
+       <div className="profText dark">
+            <p></p> Tyler Wessels
+            <p></p> Chicago
+            <p></p> Score: 20<span className="verticalLine">Likes: 5</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Menu">
+        <div className="dropdown">
+            <button className="dropbtn">Menu</button>
+            <div className="dropdown-content">
+                <a href="tb-main.html"> Home</a>
+                <a href="tb-login.html"> Sign out</a>
+            </div>
+        </div>
+      </div>
+  
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div>
+          <img className="logo" src={logo} alt=""/>
+        </div>
+        
+
+        <Menu />
+
+        <User />
+
+
+        <footer className="foot">
+            <p> Developed by Jewell Finder and Tyler Wessels </p>
+        </footer>
+
       </div>
-    );
+    )
   }
 }
 
