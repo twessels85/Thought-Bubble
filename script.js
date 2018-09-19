462a67f4fc0775c1291a249102518c4807fdb7a6
Finder <jewell.finder@parivedasolutions.com>
2018-09-19 11:21:35 -0500

    Bubbles disapear when popped and can't open a quote when clicking on the div.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var d =  document.getElementById("bubbles");
 var e = document.getElementById("expandBubble");
 
-d.addEventListener("click", function(){
-    e.classList.toggle("bubbleOn");
-    e.classList.toggle("bubbleOff");
-    d.classList.toggle("bubbleOff");
+d.addEventListener("click", function(clicked){
+    curr = clicked.target;
+    if(curr !== d){
+        e.classList.toggle("bubbleOn");
+        e.classList.toggle("bubbleOff");
+        curr.classList.toggle("bubbleOff");
+    }
+    
 });
 
 e.addEventListener("click", function(){
     e.classList.toggle("bubbleOff");
     e.classList.toggle("bubbleOn");
 });
-
