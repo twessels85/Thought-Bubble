0e745ece032b8e4b6d2076c69e4ef86acb879e8e
twessels85 <twessels85@gmail.com>
2018-09-20 15:30:38 -0500

    Cleaning

--- react-bubble/bubble/src/NewThought.js
+++ react-bubble/bubble/src/NewThought.js
@@ -4,7 +4,6 @@ import './App.css';
 class NewThought extends Component {
     render() {
         return(
-<<<<<<< HEAD
             <div className="testcontainer modalTop">
                 <form action="#">
                     <h5 className="modalItem">What inspires you today?</h5>
@@ -14,9 +13,6 @@ class NewThought extends Component {
             </div>
             /*<div id="myModal" className="modal">
                 <div className="modal-content">
-=======
-                <div className="modal modal-content">
->>>>>>> 7eebdcaca1ec348034ae3588a395b02c98879f55
                     <span className="close">&times;</span>
                     <div className="testcontainer">
                         <h5 className="modalItem">What inspires you today?</h5>
@@ -26,10 +22,7 @@ class NewThought extends Component {
                     </form>
                     </div>
                 </div>
-<<<<<<< HEAD
             </div>*/
-=======
->>>>>>> 7eebdcaca1ec348034ae3588a395b02c98879f55
         );
     }
 }
