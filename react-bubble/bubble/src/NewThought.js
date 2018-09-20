0d16785ca49a6dd18be33e0778e37659cca8fc43
twessels85 <twessels85@gmail.com>
2018-09-20 14:54:03 -0500

    Fixed NewThought

--- react-bubble/bubble/src/NewThought.js
+++ react-bubble/bubble/src/NewThought.js
@@ -4,18 +4,25 @@ import './App.css';
 class NewThought extends Component {
     render() {
         return(
-            <div id="myModal" className="modal">
+            <div className="testcontainer modalTop">
+                <form action="#">
+                    <h5 className="modalItem">What inspires you today?</h5>
+                    <textarea className="modalItem" name="userThought" rows="6" cols="40" placeholder="New thought...."></textarea>
+                    <div className="submitItem modalItem"><input className="subButton" type="submit" value="Submit"/></div>
+                </form>
+            </div>
+            /*<div id="myModal" className="modal">
                 <div className="modal-content">
                     <span className="close">&times;</span>
                     <div className="testcontainer">
-                    <h5 className="modalItem">What inspires you today?</h5>
-                    <form action="#">
-                        <textarea className="modalItem" name="userThought" rows="6" cols="40" placeholder="New thought...."></textarea>
-                        <div className="submitItem modalItem"><input className="subButton" type="submit" value="Submit"/></div>
+                        <h5 className="modalItem">What inspires you today?</h5>
+                        <form action="#">
+                            <textarea className="modalItem" name="userThought" rows="6" cols="40" placeholder="New thought...."></textarea>
+                            <div className="submitItem modalItem"><input className="subButton" type="submit" value="Submit"/></div>
                     </form>
                     </div>
                 </div>
-            </div>
+            </div>*/
         );
     }
 }
