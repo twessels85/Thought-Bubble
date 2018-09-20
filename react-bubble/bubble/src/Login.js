ee8dd1ef47a1b5fc877fadf855a311b3b549264b
Finder <jewell.finder@parivedasolutions.com>
2018-09-20 10:48:43 -0500

    Made login page, needs to be populated

---
+++ react-bubble/bubble/src/Login.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import Profile from './Profile.js';
+import Main from './Main.js';
+
+class Form extends Component {
+    render() {
+        return( 
+            <form> </form>
+        );
+    }
+}
+
+class Login extends Component {
+  render() {
+    return (
+      <Form />
+    )
+  }
+}
+
+export default Login;
