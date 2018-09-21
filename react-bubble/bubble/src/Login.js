e3b0e7ed2c9e76868bb1c302af1f55e018cfabed
twessels85 <twessels85@gmail.com>
2018-09-21 09:25:26 -0500

    Small cosmetic changes

--- react-bubble/bubble/src/Login.js
+++ react-bubble/bubble/src/Login.js
@@ -4,7 +4,7 @@ import './App.css';
 class Form extends Component {
     render() {
         return( 
-            <form action="tb-main.html" target="_self" className="testcontainer">
+            <form action="tb-main.html" target="_self" className="testcontainer loginCont">
                 <div className="loginItem"><input type="text" name="username" placeholder="Username"/></div>
                 <div className="loginItem"><input type="password" name="password" placeholder="Password"/></div>
                 <div className="submitItem modalItem"><input className="subButton" type="submit" value="Log In"/></div>
